a6b59e7c8bd94c3473e7aa0a51922c80682d9277
jacinth91 <joy237@gmail.com>
2024-07-09 15:24:59 +0400

    initial_commit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { SidebarComponent } from './app/core/components/shared/sidebar/sidebar.component';
+import { HeaderComponent } from './app/core/components/shared/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[SidebarComponent,HeaderComponent,RouterModule],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <div class="flex h-screen">
+  <app-header class="w-30"></app-header>
+  <div class="flex-1 flex flex-col">
+    <main class="flex-1 p-4 bg-gray-100">
+      <router-outlet></router-outlet>
+    </main>
+  </div>
+  <app-sidebar class="w-64"></app-sidebar>
+</div>
   `,
 })
 export class App {
