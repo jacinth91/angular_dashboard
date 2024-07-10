e69624999381b6643e138f3d4e167e87f013ad23
jacinth91 <joy237@gmail.com>
2024-07-10 19:39:11 +0400

    added prime ng

--- src/app/core/components/shared/features/components/task-assigned/task-assigned.component.ts
+++ src/app/core/components/shared/features/components/task-assigned/task-assigned.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-task-assigned',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './task-assigned.component.html',
   styleUrl: './task-assigned.component.css'
 })
 export class TaskAssignedComponent {
+  products: any[] = [
+    {
+      category: "Accessories",
+      name: "Bamboo Watch",
+      quantity: 24,
+      code: "f230fh0g3",
 
+    },
+    {
+      category: "Accessories",
+      name: "Bamboo Watch",
+      quantity: 24,
+      code: "f230fh0g3",
+
+    },
+    {
+      category: "Accessories",
+      name: "Bamboo Watch",
+      quantity: 24,
+      code: "f230fh0g3",
+
+    }
+  ]
 }
